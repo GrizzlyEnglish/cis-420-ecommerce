54ea9fba34d9335f5b83e963ea693e5cf53a5212
Alex Mattingly <ammatt12@louisville.edu>
2019-02-19 17:14:11 -0500

    added stuff

--- sk.app.js
+++ sk.app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3004;
 
 //Base logger function
 app.use(function(req, res, next) {

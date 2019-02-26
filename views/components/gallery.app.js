4db6966f275ab957468cfa9c7ad3ad5182f2952f
BrendonMac86 <brendonmac86@gmail.com>
2019-02-26 16:51:19 -0500

    added things

--- views/components/gallery.app.js
+++
@@ -1,3 +0,0 @@
-Vue.component('sk-gallery', {
-    template: '<h1>Hello, I am the gallery page</h1>'
-});
---
+++ views/components/gallery/gallery.app.js
@@ -0,0 +1,8 @@
+Vue.component('sk-gallery', {
+    template: `
+        <div class="container">
+            <sk-gallery-image href="https://bulma.io/images/placeholders/256x256.png">
+            </sk-gallery-image>
+        </div>
+    `
+});
